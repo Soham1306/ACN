112701b382dcffffc399a1aeaacaf3746df5542e
Soham1306 <sohamc388@gmail.com>
2025-04-11 08:20:12 +0530

    Delete authentication directory

--- authentication/shared_key.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Generate the key
-key = Fernet.generate_key()
-
-# Print the key so you can copy it
-print("🔑 Generated Fernet key:")
-print(key.decode())
